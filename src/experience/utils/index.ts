ea87a1af966a4b9d7e4f7a138436623a7fcf6abd
maxirpark <maxirutipark@gmail.com>
2023-07-20 21:29:15 +0900

    fix deploy errors

--- src/experience/utils/index.ts
+++ src/experience/utils/index.ts
@@ -1,9 +1,8 @@
-import { Time, TimeInt } from "./Time";
-import { Sizes, SizesInt } from "./Sizes";
-import { Camera, CameraInt } from "../Camera";
-import { Renderer, RendererInt } from "../Renderer";
+import { Time } from "./Time";
+import { Sizes } from "./Sizes";
+import { Camera } from "../Camera";
+import { Renderer } from "../Renderer";
 import PhysicsWorld from "./Physics";
 import Debug from "./Debug";
 
 export { Time, Sizes, Camera, Renderer, Debug, PhysicsWorld };
-export type { TimeInt, SizesInt, CameraInt, RendererInt };
